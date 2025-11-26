a202fb91d24abfbbc2f02c6f736558df05279263
Seungpyo Hong <rush94434@gmail.com>
2025-11-26 15:18:08 +0900

    feat: AVPlayerTests (Unit Testing Bundle) 추가

---
+++ AVPlayerTests/AVPlayerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AVPlayerTests.swift
+//  AVPlayerTests
+//
+//  Created by 홍승표 on 11/26/25.
+//
+
+import Testing
+
+struct AVPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
